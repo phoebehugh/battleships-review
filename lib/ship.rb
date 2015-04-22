d1cac934fc41f3aae779d50a442057380f86da8a
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-22 13:22:28 +0100

    Can initialize all types of ships with different sizes

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,15 +1,43 @@
 class Ship
 
-  def initialize
-    @floating = true
+  attr_reader :size
+  DEFAULT_SHIP_SIZE = 1
+  p self
+
+  def initialize(options)
+    @size = options.fetch(:size, DEFAULT_SHIP_SIZE)
+    @hits = 0
   end
 
-  def floating?
-    @floating
+  def hit
+    @hits += 1
   end
 
-  def sink!
-    @floating = false
+  def sunk?
+    @hits >= @size
   end
+
+  def self.battleship
+    new({size: 4})
+  end
+
+  def self.aircraft_carrier
+    new({size: 5})
+  end
+
+  def self.submarine
+    new({size: 3})
+  end
+
+  def self.cruiser
+    new({size: 3})
+  end
+
+  def self.destroyer
+    new({size: 2})
+  end
+
+  private
+  attr_reader :hits
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,14 +2,44 @@ require 'ship'
 
 describe Ship do
   
-  let(:ship) { Ship.new }
+  let(:ship) { Ship.new({size: 2}) }
 
-  it 'is not hit when created' do
-    expect(ship).to be_floating
+  it 'can have a size' do
+    expect(ship.size).to eq 2
   end
 
-  it 'is not floating once sunk' do
-    ship.sink!
-    expect(ship).not_to be_floating
+  it 'is not sunk when created' do
+    expect(ship).not_to be_sunk
+  end
+
+  it 'can be sunk' do
+    ship.hit
+    ship.hit
+    expect(ship).to be_sunk
+  end
+
+  it 'can initialize a battleship' do
+    battleship = Ship.battleship
+    expect(battleship.size).to eq 4
+  end
+
+  it 'can initialize an aircraft carrier' do
+    aircraft_carrier = Ship.aircraft_carrier
+    expect(aircraft_carrier.size).to eq 5
+  end
+
+  it 'can initialize a submarine' do
+    submarine = Ship.submarine
+    expect(submarine.size).to eq 3
+  end
+
+  it 'can initialize a cruiser' do
+    cruiser = Ship.cruiser
+    expect(cruiser.size).to eq 3
+  end
+
+  it 'can initialize a destroyer' do
+    destroyer = Ship.destroyer
+    expect(destroyer.size).to eq 2
   end
 end
