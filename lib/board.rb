b331c410c1f1671856f08df3c890710192b5e6e5
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-23 14:46:59 +0100

    can place ships on the grid

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,15 @@ class Board
 
   attr_reader :grid
   DEFAULT_SIZE = 1
+  DEFAULT_CONTENT = String
 
   def initialize options
     size = options.fetch(:size, DEFAULT_SIZE)
-    cell = options[:cell]
+    cell = options.fetch(:cell, DEFAULT_CONTENT)
     @grid = {}
     letter_range_based_on_size(size).map do |letter| 
       (1..dimension_size(size)).map{|number| @grid["#{letter}#{number}".to_sym] = cell.new}
     end
-
-    p grid
   end
 
   def dimension_size size
@@ -24,6 +23,10 @@ class Board
 
   def to_letter_in_alphabet number
     (number.ord + 64).chr
+  end
+
+  def place ship, coordinate
+    grid[coordinate].content= ship 
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,20 @@ require 'board'
 
 describe Board do
   
-  let(:cell){double :cell}
+  let(:cell){double :first_cell}
+  let(:second_cell){double :second_cell}
   let(:cell_class){double :cell_class, :new => cell}  
   let(:board){Board.new({size: 100, cell: cell_class})}
+  let(:ship){double :ship, size: 1}
 
   it 'has a size of 100 when created' do
     expect(board.grid.count).to eq 100
   end
 
+  it 'can place a horizontal ship of 3' do
+    board.grid[:A1] = second_cell
+    expect(second_cell).to receive(:content=).with ship
+    board.place ship, :A1
+  end
 
 end
