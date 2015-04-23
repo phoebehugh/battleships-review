cfdfd1aafebc408a6bc27290c3fb4aa3d6b4a195
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-23 13:55:32 +0100

    Uses doubles

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ describe Board do
   
   let(:cell){double :cell}
   let(:cell_class){double :cell_class, :new => cell}  
-  let(:board){Board.new({size: 100, cell: Cell})}
+  let(:board){Board.new({size: 100, cell: cell_class})}
 
   it 'has a size of 100 when created' do
     expect(board.grid.count).to eq 100
